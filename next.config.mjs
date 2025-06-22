06931ae737e334f0e366c70d08e66b8c463d16e5
weiedukg <nihuaokok@outlook.com>
2025-06-22 17:27:02 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,14 +6,8 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
-  },
+  // 因为所有图片都来自本地 public 文件夹，所以 images 配置可以完全移除
+  // Next.js 默认支持这种情况
   async redirects() {
     return [
       {
