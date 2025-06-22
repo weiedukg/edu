f0d485a007177a35d1f289c965ade96c32b72189
weiedukg <nihuaokok@outlook.com>
2025-06-22 16:32:15 +0800

    Delete weic-website directory

--- weic-website/app/[lang]/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="min-h-screen flex items-center justify-center">
-      <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-primary"></div>
-    </div>
-  )
-}
